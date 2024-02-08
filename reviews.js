16e53f3698f0516b96cac2eb1d487ba8c8f49a30
gitshubhsss <shubhamranjane16@gmail.com>
2024-02-08 20:03:03 +0530

    use of while loop for the slide bar

--- reviews.js
+++ reviews.js
@@ -1,22 +1,28 @@
-const slides =document.querySelectorAll(".slide");
-let cnt=0;
+const slides = document.querySelectorAll(".slide");
+let range = slides.length;
+let cnt = 0;
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
 
-slides.forEach((slide,index)=>{
-  slide.style.left=`${index * 100}%`;
-})
-
-const goPrev=()=>{
-  cnt--;
-  slideImage();
+const goPrev = () => {
+  while (cnt != 0) {
+    cnt--;
+    slideImage();
+    break;
+  }
   
-}
-const goNext=()=>{
-  cnt++;
-  slideImage();
-}
+};
+const goNext = () => {
+  while (cnt != range - 1) {
+    cnt++;
+    slideImage();
+    break;
+  }
+};
 
-let slideImage=()=>{
-  slides.forEach((slide)=>{
-    slide.style.transform=`translateX(-${cnt*100}%)`
-  })
-}
+let slideImage = () => {
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${cnt * 100}%)`;
+  });
+};
